508ff54584b880570123a29acecf0a61d558a810
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2020-10-15 20:30:22 +0200

    Use new set-env syntax in Actions workflow

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test 'should get new' do
+    get sessions_new_url
+    assert_response :success
+  end
+end
