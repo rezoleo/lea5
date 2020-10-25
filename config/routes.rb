e93804f5bffa40a7c429dad375cf83f6acdfd3ef
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2020-10-25 19:02:40 +0100

    Remove temporary Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
