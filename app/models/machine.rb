8fc37572fb9be0eb000a6720296a067c58d18932
Léo Salé <leo.sale72@gmail.com>
2020-11-23 21:18:20 +0100

    Validate format and uniquness of mac

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,5 +2,7 @@
 
 class Machine < ApplicationRecord
   validates :name, presence: true, allow_blank: false
-  validates :mac, presence: true, allow_blank: false
+  VALID_MAC_REGEX = /\A((\h{2}:){5}\h{2}|(\h{2}-){5}\h{2}|(\h{2}){5}\h{2})\z/i.freeze
+  validates :mac, presence: true, format: { with: VALID_MAC_REGEX },
+                  uniqueness: { unless: ->(machine) { machine.errors.include?(:mac) } }
 end
--- test/models/machine_test.rb
+++ test/models/machine_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class MachineTest < ActiveSupport::TestCase
   def setup
     @machine = Machine.new(name: 'Machine-1',
-                           mac: 'AD:12:A8:F6:45')
+                           mac: 'AD:12:A8:F6:45:BD')
   end
 
   test 'machine is valid' do
@@ -27,8 +27,37 @@ class MachineTest < ActiveSupport::TestCase
     assert_not @machine.valid?
   end
 
-  test "mac can 't be empty" do
+  test "mac can't be empty" do
     @machine.mac = '     '
     assert_not @machine.valid?
+  end
+
+  test 'mac should be unique' do
+    duplicate_machine = @machine.dup
+    @machine.save
+    assert_not duplicate_machine.valid?
+  end
+
+  test 'mac must be of a valid format' do
+    valid_macs = %w[AD:14:D4:87:4B:D7 ba:d4:8a:54:47:3f 23:Eb:1a:3A:BC:f7
+                    AD-14-D4-87-4B-D7 ba-d4-8a-54-47-3f AD14D4874BD7 bad48a54473f]
+
+    invalid_macs = ['AD:14', # must not be too short
+                    'AD:14:D4:87:4B:', # trailing colon
+                    'AD:14:D4:87:4B:45:DE', # must not be too long
+                    'AG:14:D4:87:4B:D7', # must be hexadecimal
+                    'AD/14/D4/87/4B/D7',
+                    'AD:143:D4:87:4B:D', # must be 6 times XX
+                    'AD:14-D4:87:4B:D7', # must follow one format
+                    'AD:14D4:87:4B:D7']
+    valid_macs.each do |valid_mac|
+      @machine.mac = valid_mac
+      assert @machine.valid?, "#{valid_mac.inspect} should be valid"
+    end
+
+    invalid_macs.each do |invalid_mac|
+      @machine.mac = invalid_mac
+      assert_not @machine.valid?, "#{invalid_mac.inspect} should be invalid"
+    end
   end
 end
