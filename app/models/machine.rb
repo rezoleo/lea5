c660ca17330135900983cd6c19c2bd21411970ff
Léo Salé <leo.sale72@gmail.com>
2020-11-23 19:50:10 +0100

    Add empty/nil validation

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Machine < ApplicationRecord
+  validates :name, presence: true, allow_blank: false
+  validates :mac, presence: true, allow_blank: false
 end
--- test/models/machine_test.rb
+++ test/models/machine_test.rb
@@ -3,7 +3,32 @@
 require 'test_helper'
 
 class MachineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @machine = Machine.new(name: 'Machine-1',
+                           mac: 'AD:12:A8:F6:45')
+  end
+
+  test 'machine is valid' do
+    assert @machine.valid?
+  end
+
+  test "name can't be nil" do
+    @machine.name = nil
+    assert_not @machine.valid?
+  end
+
+  test "name can't be empty" do
+    @machine.name = '    '
+    assert_not @machine.valid?
+  end
+
+  test "mac can't be nil" do
+    @machine.mac = nil
+    assert_not @machine.valid?
+  end
+
+  test "mac can 't be empty" do
+    @machine.mac = '     '
+    assert_not @machine.valid?
+  end
 end
