7b7bc13753234cb333d0c5eab6116224a5aaafd2
Léo Salé <leo.sale72@gmail.com>
2020-11-28 16:48:02 +0100

    Seed ips

---
+++ db/migrate/20201128153354_seeds_ips.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class SeedsIps < ActiveRecord::Migration[6.0]
+  def up
+    require_relative '../seeds/ips'
+  end
+
+  def down
+    Ip.delete_all
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@
 
 User.create([{ firstname: 'Paul', lastname: 'Marcel', room: 'A123', email: 'paul.marcel@gmail.com' },
              { firstname: 'Gérard', lastname: 'Dupont', room: 'd145', email: 'xXgerardDUPONTXx@gmail.com' }])
+
+require_relative './seeds/ips'
