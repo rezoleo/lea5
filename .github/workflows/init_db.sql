3dc7b2591ce6a943698248fa80ecdb2433fe8fdf
Léo Salé <leo.sale72@gmail.com>
2020-12-25 21:00:20 +0100

    Fix error on fixtures
    Rails needs superuser privileges to disable referential integrity for fixtures to
    load in a correct order. Without this, it crashes because of foreign key constraints.

--- .github/workflows/init_db.sql
+++ .github/workflows/init_db.sql
@@ -1,7 +1,11 @@
 SELECT pg_catalog.set_config('search_path', '', false);
 
 -- sudo --user postgres createuser lea5 --createdb --pwprompt --echo
-CREATE ROLE lea5 PASSWORD 'md50235066ab783f00a60dd1ea78c9ec3b2' NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN;
+/*
+Rails needs superuser privileges to disable referential integrity for fixtures to load in a correct order.
+Without this, it crashes because of foreign key constraints
+*/
+CREATE ROLE lea5 PASSWORD 'md50235066ab783f00a60dd1ea78c9ec3b2' SUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN;
 -- sudo --user postgres createdb lea5_development --owner=lea5 --echo
 CREATE DATABASE lea5_development OWNER lea5;
 -- sudo --user postgres createdb lea5_test --owner=lea5 --echo
