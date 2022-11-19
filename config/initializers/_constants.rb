f3fca7e3597170d90a4e6cac1099448e092644f7
Letiste <leo.sale72@gmail.com>
2022-11-19 08:30:34 +0100

    add sso config

---
+++ config/initializers/_constants.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Path to login via SSO authentication
+AUTH_PATH = '/auth/keycloak'
+
+# Callback path from SSO authentication
+AUTH_CALLBACK_PATH = "#{AUTH_PATH}/callback".freeze
