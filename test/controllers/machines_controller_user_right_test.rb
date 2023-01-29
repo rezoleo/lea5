39d6692a735b6f2a36bba57b61713fdc81bb9f55
Letiste <leo.sale72@gmail.com>
2023-01-29 16:17:04 +0100

    test in controller hard limit on machine creation

--- test/controllers/machines_controller_user_right_test.rb
+++ test/controllers/machines_controller_user_right_test.rb
@@ -44,4 +44,42 @@ class MachinesControllerUserRightTest < ActionDispatch::IntegrationTest
       delete machine_url(@admin.machines.first)
     end
   end
+
+  test 'non-admin user should be able to create a machine if it is in the limit' do
+    # Override to be configuration independent
+    old_value = Object.const_get(:USER_MACHINES_LIMIT)
+    silence_warnings do
+      Object.const_set(:USER_MACHINES_LIMIT, 1)
+    end
+    @user.machines.destroy_all
+
+    machine = { name: 'ultron', mac: '66:66:66:66:66:66' }
+    assert_difference 'Machine.count', 1 do
+      post user_machines_url(@user), params: { machine: }
+    end
+
+  ensure
+    silence_warnings do
+      Object.const_set(:USER_MACHINES_LIMIT, old_value)
+    end
+  end
+
+  test 'non-admin user should not be able to create a machine if limit already reached' do
+    # Override to be configuration independent
+    old_value = Object.const_get(:USER_MACHINES_LIMIT)
+    silence_warnings do
+      Object.const_set(:USER_MACHINES_LIMIT, 0)
+    end
+    @user.machines.destroy_all
+
+    machine = { name: 'ultron', mac: '66:66:66:66:66:66' }
+    assert_raises CanCan::AccessDenied do
+      post user_machines_url(@user), params: { machine: }
+    end
+
+  ensure
+    silence_warnings do
+      Object.const_set(:USER_MACHINES_LIMIT, old_value)
+    end
+  end
 end
