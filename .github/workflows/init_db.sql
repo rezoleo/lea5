e616a3e68514e690724e9512ace1e3d5b3dd71f2
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2024-05-19 20:40:32 +0200

    Update all Actions in CI

--- .github/workflows/init_db.sql
+++ .github/workflows/init_db.sql
@@ -4,8 +4,10 @@ SELECT pg_catalog.set_config('search_path', '', false);
 /*
 Rails needs superuser privileges to disable referential integrity for fixtures to load in a correct order.
 Without this, it crashes because of foreign key constraints
+TODO: Check which permissions exactly are required and if we can avoid creating a superuser
+      (This is not that important since it is only used during tests, the production user can be a normal user)
 */
-CREATE ROLE lea5 PASSWORD 'md50235066ab783f00a60dd1ea78c9ec3b2' SUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN;
+CREATE ROLE lea5 PASSWORD 'SCRAM-SHA-256$4096:0vqJhONUNwcuf1qtMlZSLA==$EI4D9sE2vsYuQRMlGVlfNQeZjryX951jo5fx44A6iOg=:c7Pk+AZ3mMNmnxs9aGP8+MtxPWLyfl2kPyrQr40ILoY=' SUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN;
 -- sudo --user postgres createdb lea5_development --owner=lea5 --echo
 CREATE DATABASE lea5_development OWNER lea5;
 -- sudo --user postgres createdb lea5_test --owner=lea5 --echo
