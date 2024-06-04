0009cb8d5e8b060f043afa048a22ce5d90b49641
Baptiste DROUET <diofun619@gmail.com>
2024-06-04 10:35:25 +0200

    feat: implements search feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 class SearchController < ApplicationController
+  VALID_IP_REGEX = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/i
   def search
     @query = params[:q]
-    @users = User.where(
+    @users = User.accessible_by(current_ability).where(
       'firstname ILIKE :search OR lastname ILIKE :search OR email ILIKE :search OR room ILIKE :search',
       search: "%#{User.sanitize_sql_like @query}%"
     )
-    @machines = Machine.where(
+    @machines = Machine.accessible_by(current_ability).where(
       'name ILIKE :search OR CAST(mac as varchar) ILIKE :search',
       search: "%#{Machine.sanitize_sql_like @query}%"
     )
-    @ip = Ip.where(ip: @query).includes(:machine).first
+    return unless @query.match?(VALID_IP_REGEX)
+
+    @ip = Ip.accessible_by(current_ability).where(ip: @query).includes(:machine).first
   end
 end
