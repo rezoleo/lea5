c4ae562ad32b6c4688a85b0e180a113375c933a4
Benoit <benoitleroux41@gmail.com>
2024-06-14 19:32:15 +0200

    Adding search test for matching users/machines/ip

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -26,6 +26,24 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test 'should return coherent results for matching users' do
+    searches_and_matches = {
+      Tony: 1,
+      tony: 1,
+      to: 1,
+      Pepper: 1,
+      pe: 2
+    }
+
+    searches_and_matches.each do |(usr, nbr)|
+      assert_msg = "should return #{nbr} result(s) for #{usr}"
+      get search_path, params: { q: usr }
+      assert_response :success
+      assert_match 'Results among users:', @response.body, assert_msg
+      assert_dom '.user', { count: nbr, text: /.*#{Regexp.escape(usr)}.*/mi }
+    end
+  end
+
   test 'should return nothing for non matching mac' do
     non_matching_macs = ['DE:1A:B3:17:95:B9', 'DC:1A:B3:17:95:B9']
 
@@ -33,6 +51,25 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
       get search_path, params: { q: mac }
       assert_response :success
       assert_no_match 'Results among machines:', @response.body, "should return nothing for #{mac} (mac not in DB)"
+    end
+  end
+
+  test 'should return coherent results for matching mac' do
+    searches_and_matches = {
+      C9: 1,
+      B8: 1,
+      c9: 1,
+      b8: 1,
+      'DE:1A:B3:17:95': 2,
+      jarvis: 1
+    }
+
+    searches_and_matches.each do |(mac, nbr)|
+      assert_msg = "should return #{nbr} result(s) for #{mac}"
+      get search_path, params: { q: mac }
+      assert_response :success
+      assert_match 'Results among machines:', @response.body, assert_msg
+      assert_dom '.machine-search', { count: nbr, text: /.*#{Regexp.escape(mac)}.*/mi }
     end
   end
 
@@ -46,14 +83,14 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  # TODO: test unicity of result
   test 'should return a unique machine for matching ips' do
-    valid_ips = ['172.130.0.0']
+    valid_ips = ['172.130.0.1', '172.130.0.0']
 
     valid_ips.each do |query|
       get search_path, params: { q: query }
       assert_response :success
       assert_match 'Results among machines:', @response.body, "should return something for #{query}"
+      assert_dom '.machine-search', { count: 1, text: /.*#{Regexp.escape(query)}.*/ }
     end
   end
 end
