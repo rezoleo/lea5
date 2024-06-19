618e3f8908ecff6320d78303ad53fd436a472ace
Benoit <benoitleroux41@gmail.com>
2024-06-19 16:58:09 +0200

    perf: skipping sql query for empty search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,10 @@
 class SearchController < ApplicationController
   VALID_IP_REGEX = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/i
   def search
-    @query = params[:q] || ''
+    @query = params[:q]
+
+    return if @query.blank?
+
     @users = User.accessible_by(current_ability).where(
       'firstname ILIKE :search OR lastname ILIKE :search OR email ILIKE :search OR room ILIKE :search',
       search: "%#{User.sanitize_sql_like @query}%"
