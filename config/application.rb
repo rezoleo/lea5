e94a96109401f76d18bb9009e9b1b7f43cd619e2
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2024-06-20 16:00:49 +0200

    Upgrade Rails to 7.1.3.4 in Gemfile and run app:update

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,11 @@ module Lea5
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: ['assets', 'tasks'])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
