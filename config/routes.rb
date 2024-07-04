a27935a131b8e9122a2144df4df37a450092d59f
Baptiste DROUET <diofun619@gmail.com>
2024-07-04 21:34:50 +0200

    Deletion of subscriptions controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,9 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :machines, shallow: true, except: [:index]
-    # resources :subscriptions, shallow: true, only: [:new, :create]
     resources :sales, shallow: true, only: [:new, :create] do
       resources :refunds, shallow: true, only: [:new, :create]
     end
-    # delete '/last_subscription', to: 'subscriptions#destroy', as: 'last_subscription'
     resources :free_accesses, shallow: true, except: [:index, :show]
   end
 
