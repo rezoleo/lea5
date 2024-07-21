6d3196fadde2c4448e88e2d8f4cff62e7b7a06aa
D0gma_ <d0gma@d0gma.fr>
2024-07-21 17:33:09 +0200

    test: write some invoice tests

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -3,7 +3,68 @@
 require 'test_helper'
 
 class InvoiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @client = users(:ironman)
+    @sale = sales(:one)
+    @sale.client = @client
+    @invoice = invoices(:one)
+    @invoice.sale = @sale
+  end
+
+  test 'should be valid' do
+    assert_predicate @invoice, :valid?
+  end
+
+  test 'should generate correct user' do
+    assert_equal @invoice.user, @client
+  end
+
+  test 'should generate correct hash' do
+    expected_hash = {
+      sale_date: Time.zone.today,
+      issue_date: Time.zone.today,
+      client_name: @sale.client.display_name,
+      client_address: @sale.client.display_address,
+      payment_amount_cent: @sale.verified_at.nil? ? 0 : @sale.total_price,
+      payment_method: @sale.payment_method.name,
+      payment_date: @sale.verified_at,
+      items: @invoice.send(:sales_itemized, @sale)
+    }.compact
+
+    assert_equal @invoice.send(:generate_hash, @sale), expected_hash
+  end
+
+  test 'should generate json and id from sale' do
+    @invoice.generation_json = nil
+    @invoice.id = nil
+
+    @invoice.generate_from(@sale)
+
+    assert_not_nil @invoice.generation_json
+    assert_not_nil @invoice.id
+  end
+
+  test 'should create invoice with pdf' do
+    @invoice.generation_json = @invoice.send(:generate_hash, @sale).to_json
+
+    assert_difference('ActiveStorage::Attachment.count', 1) do
+      @invoice.send(:create_invoice)
+      @invoice.save!
+    end
+
+    assert_predicate @invoice.pdf, :attached?
+  end
+
+  test 'should set id if nil on create invoice' do
+    @invoice.generation_json = @invoice.send(:generate_hash, @sale).to_json
+    @invoice.id = nil
+    @invoice.send(:create_invoice)
+    assert_not_nil @invoice.id
+  end
+
+  test 'should not destroy invoice if sale exists' do
+    assert_raises(ActiveRecord::DeleteRestrictionError) do
+      @invoice.destroy
+    end
+  end
 end
