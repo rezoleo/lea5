c92c379ff81dc75e9b13b2e07595da274f33a784
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2025-01-19 18:32:20 +0100

    feat: protect Dashboard SQL queries with current user ability

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,10 +3,10 @@
 module Admin
   class DashboardController < ApplicationController
     def index
-      authorize! :manage, :all
-      @articles = Article.order(created_at: :desc)
-      @subscription_offers = SubscriptionOffer.order(created_at: :desc)
-      @payment_methods = PaymentMethod.order(created_at: :desc)
+      authorize! :manage, :all # TODO: use finer grained permissions
+      @articles = Article.accessible_by(current_ability).order(created_at: :desc)
+      @subscription_offers = SubscriptionOffer.accessible_by(current_ability).order(created_at: :desc)
+      @payment_methods = PaymentMethod.accessible_by(current_ability).order(created_at: :desc)
     end
   end
 end
