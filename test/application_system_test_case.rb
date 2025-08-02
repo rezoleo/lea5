5793cfe47ad585af538c34ec467e2aed762c633d
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2025-08-02 22:08:42 +0200

    test: skip coverage on some less-used tests helpers

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,17 +4,20 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # Inspired by https://nts.strzibny.name/rails-system-tests-headless/
+  # :nocov:
   DRIVER = if ENV['DRIVER']
              ENV['DRIVER'].to_sym
            else
              :headless_chrome
            end
+  # :nocov:
 
   # To pass options to the underlying driver, we need to use a block like so:
   # driven_by ..., |driver_option| do
   #   driver_option.something
   # end
   # https://rubydoc.info/gems/actionpack/7.0.4/ActionDispatch/SystemTestCase
+  # :nocov:
   if ENV['CAPYBARA_SERVER_PORT']
     served_by host: 'rails-app', port: ENV['CAPYBARA_SERVER_PORT']
 
@@ -22,12 +25,15 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
       browser: :remote,
       url: "http://#{ENV.fetch('SELENIUM_HOST')}:4444"
     }
+    # :nocov:
   else
     capybara_options = {}
   end
   driven_by :selenium, using: DRIVER, screen_size: [1400, 1400], options: capybara_options do |driver_option|
     # https://www.selenium.dev/documentation/webdriver/browsers/firefox/#start-browser-in-a-specified-location
+    # :nocov:
     driver_option.binary = ENV['DRIVER_BINARY'] if ENV['DRIVER_BINARY']
+    # :nocov:
   end
 
   Capybara.enable_aria_label = true
