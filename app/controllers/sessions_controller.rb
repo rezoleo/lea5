bfeb99608f63cb2e46d714f09dee7bd788a8c4b4
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2025-08-04 00:38:51 +0200

    Add local login strategy to develop without Keycloak (#464)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,21 @@ class SessionsController < ApplicationController
     redirect_to user_path user
   end
 
+  def create_developer
+    auth_hash = request.env['omniauth.auth']['info']
+    user = User.find_or_create_by!(
+      email: auth_hash[:email]
+    ) do |u|
+      u.firstname = auth_hash[:first_name]
+      u.lastname = auth_hash[:last_name]
+      u.room = auth_hash[:room]
+    end
+    user.groups = auth_hash[:groups].split(',')
+    log_in user
+    flash[:success] = 'You are now logged in (using developer method)!'
+    redirect_to user
+  end
+
   def destroy
     log_out
     flash[:success] = 'You are now logged out!'
