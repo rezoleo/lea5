2e16cde0b8f055f7526be98454186fc67c3f6e7f
alias <perettix.laurent@gmail.com>
2025-10-03 19:27:07 +0200

    refactor: moved md4 hashing step to controller instead of view

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,6 +10,9 @@ module Api
       @user = User.find(params[:id])
       authorize! :show, @user
       @machines = @user.machines.includes(:ip).order(created_at: :asc)
+      openssl_legacy_provider = OpenSSL::Provider.load('legacy')
+      @ntlm_password = OpenSSL::Digest::MD4.hexdigest(@user.wifi_password.encode('utf-16le'))
+      openssl_legacy_provider.unload
     end
   end
 end
--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -3,6 +3,4 @@
 # locals: ()
 
 json.partial! 'api/users/user', user: @user
-openssl_legacy_provider = OpenSSL::Provider.load('legacy')
-json.ntlm_password OpenSSL::Digest::MD4.hexdigest(@user.wifi_password.encode('utf-16le'))
-openssl_legacy_provider.unload
+json.ntlm_password @ntlm_password
