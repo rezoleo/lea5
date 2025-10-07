f7e9c1d1be507c4d7ec4e7eaed252703f40bbfd7
alias <perettix.laurent@gmail.com>
2025-10-07 19:13:49 +0200

    refactor: more optimal user view for password management

--- app/javascript/controllers/user_controller.js
+++ app/javascript/controllers/user_controller.js
@@ -8,12 +8,9 @@ export default class extends Controller {
     const text = this.passwordValue
     const copyMessage = this.copyMessageTarget
     navigator.clipboard.writeText(text)
-    copyMessage.classList.replace("copy-message-hidden", "copy-message-visible")
+    copyMessage.classList.add("visible")
     setTimeout(() => {
-      copyMessage.classList.replace(
-        "copy-message-visible",
-        "copy-message-hidden"
-      )
+      copyMessage.classList.remove("visible")
     }, 2000)
   }
 
@@ -22,26 +19,16 @@ export default class extends Controller {
     const eyeIcon = this.eyeIconTargets[0]
     const eyeSlashIcon = this.eyeIconTargets[1]
 
-    if (
-      passwordField.classList.contains(
-        "card-content-user-details-password-hidden"
-      )
-    ) {
-      passwordField.classList.replace(
-        "card-content-user-details-password-hidden",
-        "card-content-user-details-password-visible"
-      )
-      eyeIcon.classList.replace("eye-hidden", "eye-visible")
-      eyeSlashIcon.classList.replace("eye-visible", "eye-hidden")
-      passwordField.textContent = this.passwordValue
-    } else {
-      passwordField.classList.replace(
-        "card-content-user-details-password-visible",
-        "card-content-user-details-password-hidden"
-      )
+    if (passwordField.classList.contains("visible")) {
+      passwordField.classList.remove("visible")
+      passwordField.textContent = "********************"
       eyeIcon.classList.replace("eye-visible", "eye-hidden")
       eyeSlashIcon.classList.replace("eye-hidden", "eye-visible")
-      passwordField.textContent = "********************"
+    } else {
+      passwordField.classList.add("visible")
+      passwordField.textContent = this.passwordValue
+      eyeIcon.classList.replace("eye-hidden", "eye-visible")
+      eyeSlashIcon.classList.replace("eye-visible", "eye-hidden")
     }
   }
 }
